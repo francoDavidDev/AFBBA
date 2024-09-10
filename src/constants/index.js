b31bf9367e06d41efafb4b046fa085e09c0a3fea
franco david gomez <francodavid_dev@outlook.com>
2024-09-10 00:05:40 -0300

    ss

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,7 +17,7 @@ import assfl from '../assets/imgs/associations/assfl.png';
 
 const ASSOCIATIONS = [
   {
-    title: "Asociación Salteña de Fisicoculturismo Fitness",
+    title: "Asociación Salteña de Fisicoculturismo y Fitness",
     company_name: "Presidente",
     icon: asff,
     iconBg: "#383E56",
