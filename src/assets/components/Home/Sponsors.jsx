82632d1a55113ee46d7d6eea572f4c0254551a73
franco david gomez <francodavid_dev@outlook.com>
2024-09-26 16:52:41 -0300

    ssd

--- src/assets/components/Home/Sponsors.jsx
+++ src/assets/components/Home/Sponsors.jsx
@@ -11,11 +11,11 @@ const Sponsors = () => {
     <section className="banner mb-20">
       <div className="slider-container">
         <div className="slider">
-          <a href='https://www.veesfitness.net/en' target="_blank" className="item" style={{ '--position': 1 }}><img src={vees} alt="Vees Fitness" /></a>
+          <a href='https://www.veesfitness.com.ar/' target="_blank" className="item" style={{ '--position': 1 }}><img src={vees} alt="Vees Fitness" /></a>
           <a href="https://www.instagram.com/bsnpharmacordoba?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" className="item" style={{ '--position': 2 }}><img src={bsn} alt="BSN" /></a>
           <a href="https://www.instagram.com/sansongym_oficial/" target="_blank" className="item" style={{ '--position': 3 }}><img src={sansongym} alt="Sansongym" /></a>
           <a href="https://www.instagram.com/fitness_sportcenter23?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" className="item" style={{ '--position': 4 }}><img src={sportcenter} alt="Sportcenter" /></a>
-          <a href='https://www.veesfitness.net/en' target="_blank" className="item" style={{ '--position': 5 }}><img src={vees} alt="Vees Fitness" /></a>
+          <a href='https://www.veesfitness.com.ar/' target="_blank" className="item" style={{ '--position': 5 }}><img src={vees} alt="Vees Fitness" /></a>
           <a href="https://www.instagram.com/bsnpharmacordoba?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" className="item" style={{ '--position': 6 }}><img src={bsn} alt="BSN" /></a>
           <a href="https://www.instagram.com/sansongym_oficial/" target="_blank" className="item" style={{ '--position': 7 }}><img src={sansongym} alt="Sansongym" /></a>
           <a href="https://www.instagram.com/fitness_sportcenter23?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" className="item" style={{ '--position': 8 }}><img src={sportcenter} alt="Sportcenter" /></a>
