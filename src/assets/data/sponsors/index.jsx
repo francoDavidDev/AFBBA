5ebd5bb531647cec8feaa6ca8effdf0fa13556c5
franco david gomez <francodavid_dev@outlook.com>
2024-10-28 12:35:05 -0300

    agrege nueo espornsort

--- src/assets/data/sponsors/index.jsx
+++ src/assets/data/sponsors/index.jsx
@@ -2,26 +2,48 @@ import bsn from '../../imgs/sponsors/bsn.jpg';
 import sansongym from '../../imgs/sponsors/sansongym.jpg';
 import sportcenter from '../../imgs/sponsors/sportcenter.jpg';
 import vees from '../../imgs/sponsors/vees.jpg';
+import augeFitt from '../../imgs/sponsors/augeFitt.jpg';
+import panteraFitness from '../../imgs/sponsors/panteraFitness.jpeg';
+
 
 export const SPONSORS = [
+
+
   {
+    image: sportcenter,
+    link: "https://www.instagram.com/fitness_sportcenter23?igsh=MTNqNm9meWJ1MTZveQ==",
+    alt: "Sportcenter",
+  },
+
+  {
+    image: panteraFitness,
+    link: "https://www.instagram.com/panterafitness.ok?igsh=NGx3OWtlZnN6ZG0z",
+    alt: "Sportcenter",
+  },
+
+  {
+    image: augeFitt,
+    link: "https://www.instagram.com/augefactoryfitness/",
+    alt: "Sportcenter",
+  },
+  
+];
+
+
+
+  {/*
     image: bsn,
     link: "https://www.instagram.com/bsnpharmacordoba?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     alt: "BSN",
-  },
-  {
+  */}
+  {/*
     image: sansongym,
     link: "https://www.instagram.com/sansongym_oficial/",
     alt: "Sansongym",
-  },
-  {
-    image: sportcenter,
-    link: "https://www.instagram.com/fitness_sportcenter23?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-    alt: "Sportcenter",
-  },
-  {
+  */}
+ 
+  {/*
     image: vees,
     link: "https://www.veesfitness.net/en",
     alt: "Vees Fitness",
-  },
-];
+  */}
