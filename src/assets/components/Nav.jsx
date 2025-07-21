1c33acc356f154b4ff5cb7cdfbf0f17bb583fdb6
franco home <francodavid_dev@outlook.com>
2025-07-21 13:13:18 -0300

    s

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,3 +1,4 @@
+// components/common/Nav.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import { NAV } from "../../data";
@@ -14,8 +15,8 @@ const Nav = ({ className }) => {
   };
 
   return (
-    <nav className={`flex w-full justify-center`}>
-      <ul className="flex flex-col md:flex-row text-primary-200 font-semibold gap-y-4 md:gap-x-6">
+    <nav className={`w-full ${className}`}>
+      <ul className="flex flex-col items-center md:flex-row md:justify-center text-primary-200 font-semibold gap-4 md:gap-6 px-6 py-4">
         {NAV.map((item, index) => (
           <li
             key={index}
@@ -25,14 +26,14 @@ const Nav = ({ className }) => {
               <a
                 href={item.href}
                 onClick={(e) => handleEntradasClick(e, item.href)}
-                className="text-lg sm:text-xl md:text-base lg:text-lg"
+                className="text-lg md:text-base lg:text-lg"
               >
                 {item.name}
               </a>
             ) : (
               <Link
                 to={item.href}
-                className="text-lg sm:text-xl md:text-base lg:text-lg"
+                className="text-lg md:text-base lg:text-lg"
               >
                 {item.name}
               </Link>
